53ca6a215654efd77a9797b378ede393cf1e5c9f
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-08-11 22:41:19 +0300

    chore: mocha replaced by jest.

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js'],
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: []
+};
+
+export default config;
